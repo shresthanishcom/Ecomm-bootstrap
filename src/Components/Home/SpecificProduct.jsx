ce678e90de60c69ae481bf5311bff19dbb781593
shresthanishcom <78007932+shresthanishcom@users.noreply.github.com>
2022-02-19 13:53:16 +0545

    slicing done
    there was a problem in git so I put these copy paster from website
    git problem : git failed to push some refs

--- src/Components/Home/SpecificProduct.jsx
+++ src/Components/Home/SpecificProduct.jsx
@@ -7,22 +7,57 @@ import axios from "axios";
 function SpecificProduct(props) {
   const [products, setProducts] = useState([]);
 
+  const numberOfItems = 4; //number of items to show
+
+  const [state, setState] = useState({
+    trimmedProducts: [],
+    initialIndex: 0,
+    latestIndex: numberOfItems,
+  });
+
   useEffect(() => {
     async function getProducts() {
       await axios
         .get("http://localhost:5000/product/newproducts")
         .then((res) => {
-          setProducts(res.data);
+          const products = res.data;
+          console.log("from the server products", products);
+          setProducts(products);
+          slicer();
         })
         .catch((err) => console.log("error while getting products", err));
     }
     getProducts();
   }, []);
 
+  const slicer = () => {
+    if (products.length > numberOfItems) {
+      setState({
+        ...state,
+        trimmedProducts: products.slice(state.initialIndex, state.latestIndex),
+        initialIndex: state.initialIndex + 4,
+        latestIndex: state.latestIndex + 4,
+      });
+    } else {
+      setState({ ...state, trimmedProducts: products });
+    }
+  };
   const showProducts = () => {
-    return products.map((product) => {
+    return state.trimmedProducts.map((product) => {
       return <Product key={product._id} product={product} />;
     });
+  };
+
+  const handleButton = (e) => {
+    console.log("clicked column", e.target.name);
+    switch (e.target.name) {
+      case "next":
+        console.log("nextclick");
+        slicer();
+        break;
+      case "previous":
+        break;
+    }
   };
   return (
     <div>
@@ -37,6 +72,7 @@ function SpecificProduct(props) {
               icon={faArrowLeft}
             />
             <FontAwesomeIcon
+              name="next"
               className="mr-2 btn btn-danger"
               icon={faArrowRight}
             />
