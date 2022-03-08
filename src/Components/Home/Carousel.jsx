a698deaa4b4d043129a7e2094861ae65a4010f00
shresthanishcom <me.anish.shrestha@gmail.com>
2022-03-08 09:14:33 +0545

    caraousel work

--- src/Components/Home/Carousel.jsx
+++ src/Components/Home/Carousel.jsx
@@ -1,62 +1,12 @@
 import React from "react";
+import "../sass/Scss-Sections/_carousel.scss";
 
 function Carousel() {
   return (
-    <div
-      id="carouselExampleIndicators"
-      className="carousel slide"
-      data-ride="carousel"
-    >
-      <ol className="carousel-indicators">
-        <li
-          data-target="#carouselExampleIndicators"
-          data-slide-to="0"
-          className="active"
-        ></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img
-            className="d-block w-100"
-            src="./images/ec.png"
-            alt="First slide"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            className="d-block w-100"
-            src="./images/ec.png"
-            alt="Second slide"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            className="d-block w-100"
-            src="./images/bag.png"
-            alt="Third slide"
-          />
-        </div>
-      </div>
-      <a
-        className="carousel-control-prev"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a
-        className="carousel-control-next"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
+    <div className="carousel">
+      <img src="./images/laptop.png" alt="first slide" />
+      <img src="./images/bag.png" alt="second slide" />
+      <img src="./images/girl.png" alt="third slide" />
     </div>
   );
 }
