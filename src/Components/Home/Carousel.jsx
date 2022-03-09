29a65c7eadfdda3e3d137e385d5d9572318361f7
shresthanishcom <me.anish.shrestha@gmail.com>
2022-03-09 10:10:09 +0545

    bug

--- src/Components/Home/Carousel.jsx
+++ src/Components/Home/Carousel.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import "../sass/Scss-Sections/_carousel.scss";
 
 function Carousel() {
-  const [state, setState] = useState({ currentSlide: 0 });
+  const [state, setState] = useState({ currentSlide: 0, allItems: [] });
   useEffect(() => {
-    console.log("use Effect running");
     const allItems = document.querySelectorAll(".manual-carousel-item");
-    console.log("allitems", allItems);
+    console.log("all items in useEffect", allItems);
+    setState({ ...state, allItems: allItems });
     const automaticButton = document.getElementById("automatic-button");
     for (let i = 0; i < allItems.length; i++) {
       let newList = document.createElement("li");
@@ -16,25 +16,40 @@ function Carousel() {
       automaticButton.appendChild(newList);
     }
   }, []);
-  const handleCarouselBtnClick = (e) => {
-    const allItems = document.querySelectorAll(".manual-carousel-item");
-    const id = e.target.id;
+
+  const changeCarouselImage = (id) => {
+    const allItems = state.allItems;
+    console.log("alll items are ", allItems);
     for (let i = 0; i < allItems.length; i++) {
       allItems[i].classList.remove("active");
     }
 
     allItems[id].classList.add("active");
   };
+  const handleCarouselBtnClick = (e) => {
+    changeCarouselImage(e.target.id);
+  };
   const handleClick = (e) => {
+    let currentSlide = state.currentSlide;
     if (e.target.name === "left") {
-      setState({ ...state, currentSlide: state.currentSlide - 1 });
+      // if (currentSlide <= 0) {
+      //   currentSlide = state.allItems.length;
+      // }
+      currentSlide -= 1;
+      changeCarouselImage(currentSlide);
+      setState({ ...state, currentSlide });
     } else if (e.target.name === "right") {
+      // if (currentSlide >= state.allItems.length) {
+      //   currentSlide = state.allItems.length;
+      // }
+      changeCarouselImage(currentSlide);
       setState({ ...state, currentSlide: state.currentSlide + 1 });
     }
   };
 
   return (
     <div className="carousel-container">
+      {console.log(state)}
       <div name="left" className="left" onClick={handleClick}>
         {"<--"}
       </div>
